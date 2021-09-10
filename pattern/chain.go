0e0003ce50180555f7c75f47d8fe10fa653fcdb7
vicent-dev <vicent@vicentdev.com>
2021-09-11 00:04:55 +0200

    pattern chain

---
+++ pattern/chain.go
@@ -0,0 +1,20 @@
+package pattern
+
+type ChainInterface interface {
+	Process(data interface{}) interface{}
+}
+
+type Chain struct {
+	services []ChainInterface
+}
+
+func NewChain(services ...ChainInterface) *Chain {
+	return &Chain{services: services}
+}
+
+func (c *Chain) Process(data interface{}) interface{} {
+	for _, s := range c.services {
+		data = s.Process(data)
+	}
+	return data
+}
---
+++ pattern/chain_test.go
@@ -0,0 +1,49 @@
+package pattern
+
+import (
+	"testing"
+)
+
+type payload struct {
+	n1, n2, result float64
+}
+
+type sum struct{}
+
+func (s sum) Process(data interface{}) interface{} {
+	d := data.(payload)
+	d.result = d.n1 + d.n2
+	return d
+}
+
+type multiplier struct{}
+
+func (m multiplier) Process(data interface{}) interface{} {
+	d := data.(payload)
+	d.result += d.n1 * d.n2
+	return d
+}
+
+type subtraction struct{}
+
+func (s subtraction) Process(data interface{}) interface{} {
+	d := data.(payload)
+	d.result += d.n1 - d.n2
+	return d
+}
+
+func TestChain_Process(t *testing.T) {
+	c := NewChain(
+		sum{},
+		multiplier{},
+		subtraction{},
+	)
+
+	data := c.Process(payload{1, 2, 0}).(payload)
+	expected := (1 + 2) + (1 * 2) + (1 - 2)
+
+	if data.result != float64(expected) {
+		t.Errorf("Expected %v but get %v", expected, data.result)
+	}
+
+}
